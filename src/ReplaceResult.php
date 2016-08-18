7f2582118dac1273138c73e429e278822ce85ef5
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-08-18 12:33:42 +0200

    ReplaceResult refactor

--- src/ReplaceResult.php
+++ src/ReplaceResult.php
@@ -32,18 +32,11 @@ class ReplaceResult extends RegexResult
 
     public static function for($pattern, $replacement, $subject, $limit)
     {
-        $count = 0;
-
         try {
-            if (is_callable($replacement)) {
-                $replacement = function (array $matches) use ($pattern, $subject, $replacement) {
-                    return $replacement(new MatchResult($pattern, $subject, true, $matches));
-                };
+            list($result, $count) = is_callable($replacement) ?
+                static::doReplacementWithCallable($pattern, $replacement, $subject, $limit) :
+                static::doReplacement($pattern, $replacement, $subject, $limit);
 
-                $result = preg_replace_callback($pattern, $replacement, $subject, $limit, $count);
-            } else {
-                $result = preg_replace($pattern, $replacement, $subject, $limit, $count);
-            }
         } catch (Exception $exception) {
             throw RegexFailed::replace($pattern, $subject, $exception->getMessage());
         }
@@ -53,6 +46,28 @@ class ReplaceResult extends RegexResult
         }
 
         return new static($pattern, $replacement, $subject, $result, $count);
+    }
+
+    protected static function doReplacement($pattern, $replacement, $subject, $limit)
+    {
+        $count = 0;
+
+        $result = preg_replace($pattern, $replacement, $subject, $limit, $count);
+
+        return [$result, $count];
+    }
+
+    protected static function doReplacementWithCallable($pattern, callable $replacement, $subject, $limit)
+    {
+        $replacement = function (array $matches) use ($pattern, $subject, $replacement) {
+            return $replacement(new MatchResult($pattern, $subject, true, $matches));
+        };
+
+        $count = 0;
+
+        $result = preg_replace_callback($pattern, $replacement, $subject, $limit, $count);
+
+        return [$result, $count];
     }
 
     public function result()
