5129a43d68fcfe7716fc362ca51a0ceff70b9e29
Guilherme Pressutto <gpressutto5@gmail.com>
2018-10-26 13:52:55 -0300

    Fix determining the last preg error, broken by #31

--- src/RegexResult.php
+++ src/RegexResult.php
@@ -2,10 +2,17 @@
 
 namespace Spatie\Regex;
 
+use Spatie\Regex\Helpers\Str;
+
 abstract class RegexResult
 {
     protected static function lastPregError(): string
     {
-        return array_search(preg_last_error(), get_defined_constants(true)['pcre'], true);
+        $pcreConstants = get_defined_constants(true)['pcre'];
+        $pcreErrors = array_filter($pcreConstants, function ($errorMessage) {
+            return Str::endsWith($errorMessage, '_ERROR');
+        }, ARRAY_FILTER_USE_KEY);
+
+        return array_search(preg_last_error(), $pcreErrors, true);
     }
 }
