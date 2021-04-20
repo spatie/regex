27e73ffb1ceabac0ccc0b6d8f44b886f314f539a
Markus Staab <maggus.staab@googlemail.com>
2021-04-20 10:28:07 +0200

    Str::ends_with: re-added early-if-return

--- src/Helpers/Str.php
+++ src/Helpers/Str.php
@@ -6,6 +6,12 @@ class Str
 {
     public static function endsWith(string $haystack, string $needle): bool
     {
+        // str_ends_with() will return false for an empty string
+        $length = strlen($needle);
+        if ($length == 0) {
+            return true;
+        }
+        
         return str_ends_with($haystack, $needle);
     }
 }
