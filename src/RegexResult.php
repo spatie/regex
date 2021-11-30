ddc650914b3be5a637d6d97ea4c6891ab2ef18a7
Jordi Boggiano <j.boggiano@seld.be>
2021-11-30 21:56:27 +0100

    Use nicer error messages

--- src/RegexResult.php
+++ src/RegexResult.php
@@ -8,13 +8,6 @@ abstract class RegexResult
 {
     protected static function lastPregError(): string
     {
-        $pcreConstants = get_defined_constants(true)['pcre'];
-        $pcreErrors = array_filter(
-            $pcreConstants,
-            fn ($errorMessage) => Str::endsWith($errorMessage, '_ERROR'),
-            ARRAY_FILTER_USE_KEY
-        );
-
-        return array_search(preg_last_error(), $pcreErrors, true);
+        return preg_last_error_msg();
     }
 }
